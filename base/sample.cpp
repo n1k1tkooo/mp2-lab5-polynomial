e4b4b9ba80dd9ced5f14120882dd49af49ffb47e
Nikita <71322742+n1k1tkooo@users.noreply.github.com>
2022-12-27 23:51:08 +0300

    ver 1.0
    Solution

---
+++ base/sample.cpp
@@ -0,0 +1,63 @@
+﻿#include "Polinom.h"
+#include "string.h"
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	string pol1, pol2;
+	cout << "Введите первый полином: ";
+	cin >> pol1;
+	cout << "Введите второй полином: ";
+	cin >> pol2;
+	string chance;
+	int x = 0, y = 0, z = 0;
+	int calAddition, calSubtraction, calMultiplication;
+	TPolinom polinom1(pol1), polinom2(pol2);
+	TPolinom addition, subtraction, multiplication;
+	polinom1.Convert();
+	polinom2.Convert();
+	addition = polinom1 + polinom2;
+	multiplication = polinom1 * polinom2;
+	subtraction = polinom1 - polinom2;
+	cout << "Хотите ли Вы вывести произведенные вычисления? Введите 'yes' или 'no'." << endl;
+	cin >> chance;
+	if (chance == "yes")
+	{
+		cout << "Первый полином: F = " << polinom1 << endl;
+		cout << "Второй полином: G = " << polinom2 << endl;
+		cout << "Умножение F*G = " << multiplication << endl;
+		cout << "Вычитание F-G = " << subtraction << endl;
+		cout << "Сложение F+G = " << addition << endl;
+	}
+	cout << "Хотите ли Вы найти значение в точке? Введите 'yes' или 'no'." << endl;
+	cin >> chance; 
+	if (chance == "yes")
+	{
+		cout << "Введите значение переменной x: " << endl;
+		cin >> x;
+		cout << "Введите значение переменной y: " << endl;
+		cin >> y;
+		cout << "Введите значение переменной z: " << endl;
+		cin >> z;
+		cout << "Первый полином: F = " << polinom1 << endl;
+		cout << "Второй полином: G = " << polinom2 << endl;
+		calAddition = addition.CalculateInPoint(x, y, z);
+		cout << "F+G(" << x << "," << y << "," << z << ") = " << calAddition << endl;
+		calMultiplication = multiplication.CalculateInPoint(x, y, z);
+		cout << "F*G(" << x << "," << y << "," << z << ") = " << calMultiplication << endl;
+		calSubtraction = subtraction.CalculateInPoint(x, y, z);
+		cout << "F-G(" << x << "," << y << "," << z << ") = " << calSubtraction << endl;
+	}
+	cout << "Хотите ли Вы сохранить результаты в файл? Введите 'yes' или 'no'." << endl;
+	cin >> chance;
+	if (chance == "yes")
+	{
+		if (x==0 && y==0 && z==0)
+			polinom1.SaveInFile(polinom1, polinom2, multiplication, subtraction, addition);
+		else
+			polinom1.SaveInFile(polinom1, polinom2, multiplication, calMultiplication, subtraction, calSubtraction, addition, calAddition, x, y, z);
+	}
+	cout << "Хотите ли Вы считать из файла произведенные вычисления? Введите 'yes' или 'no'." << endl;
+	cin >> chance;
+	if (chance == "yes")
+		polinom1.ReadFile();
+}
---
+++ base_test/test_tpolinom.cpp
@@ -0,0 +1,63 @@
+﻿#include "Polinom.cpp"
+#include <gtest.h>
+
+TEST(TPolinom, can_create_polinom)
+{
+  ASSERT_NO_THROW(TPolinom polinom);
+}
+TEST(TPolinom, can_create_polinom_with_argument)
+{
+	TPolinom polinom1;
+	ASSERT_NO_THROW(TPolinom polinom2(polinom1));
+}
+TEST(TPolinom, can_create_polinom_with_monom)
+{
+	ASSERT_NO_THROW(TPolinom polinom("3x4y5z6"));
+}
+TEST(TPolinom, can_calculate_in_point_of_polinom)
+{
+	TPolinom polinom("3x4y5z6-2x5yz3");
+	polinom.Convert();
+	int result = polinom.CalculateInPoint(1, 1, 1);
+	EXPECT_EQ(1, result);
+}
+TEST(TPolinom, can_compare_prefix_of_two_polinom)
+{
+	TPolinom polinom("3x4y5z6-2x5yz3");
+	EXPECT_EQ("3x4y5z6-2x5yz3", polinom.GetPrefix());
+}
+TEST(TPolinom, can_compare_two_polinom_with_identical_value)
+{
+	TPolinom polinom1("3x4y5z6-2x5yz3");
+	TPolinom polinom2("3x4y5z6-2x5yz3");
+	EXPECT_TRUE(polinom1 == polinom2);
+}
+TEST(TPolinom, can_compare_two_polinom_with_different_value)
+{
+	TPolinom polinom1("3x4y5z6-2x5yz3");
+	TPolinom polinom2("3x4y5z6+2x5yz3");
+	EXPECT_FALSE(polinom1 == polinom2);
+}
+TEST(TPolinom, cant_sort_empty_polinom)
+{
+	TPolinom polinom;
+	ASSERT_ANY_THROW(polinom.Sort());
+}
+TEST(TPolinom, cant_addition_with_empty_polinom)
+{
+	TPolinom polinom1("3x4y5z6-2x5yz3");
+	TPolinom polinom2("");
+	ASSERT_ANY_THROW(polinom1+polinom2);
+}
+TEST(TPolinom, cant_subtraction_with_empty_polinom)
+{
+	TPolinom polinom1("3x4y5z6-2x5yz3");
+	TPolinom polinom2("");
+	ASSERT_ANY_THROW(polinom1 - polinom2);
+}
+TEST(TPolinom, cant_multiplication_with_empty_polinom)
+{
+	TPolinom polinom1("3x4y5z6-2x5yz3");
+	TPolinom polinom2("");
+	ASSERT_ANY_THROW(polinom1 * polinom2);
+}
